f6a241f62ba5e7a6b84b4453da0172ec75dff5a0
unknown <harp3133t@gmail.com>
2021-09-03 16:17:38 +0900

    비밀 정보 보호 실습

--- index.js
+++ index.js
@@ -2,14 +2,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { User } = require('./models/User')
+const bodyParser = require('body-parser')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
+const config = require('./config/key')
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://harp3133t:9h4t2x!@@cluster.648on.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World11!')
+})
+
+app.post('/register', (req,res) => {
+  // 회원가입할때 필요한 정보들을 client 에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
